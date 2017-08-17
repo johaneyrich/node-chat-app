c68922d4a330950a49832f1599c4a725c6310cb5
Johan.Eyrich <johan@eyrich.dk>
2017-08-17 08:21:55 +0200

    origin master

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+//på heroko vil env være sæt til production
+var env = process.env.NODE_ENV || 'development';
+console.log('env *** ', env);
+
+if (env  === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+
+}
---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('./config/config.js');
+
+const express = require('express');
+
+const path = require('path');
+const publicPath = path.join(__dirname + '/../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+
+app.use(express.static(publicPath));
+
+
+app.listen(port , () => {
+  console.log('Started on port ' + port);
+});
+
+module.exports = {app};
